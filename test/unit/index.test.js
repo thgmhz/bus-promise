d3bb1273f5949eb73385b31a90929c5274b1bb89
= <thiagomunhozmedeiros@gmail.com>
2017-04-07 00:46:08 -0300

    Update test description

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,6 +1,6 @@
 import test from 'ava'
 import spbus from '../../src'
 
-test('when imported should return a Function', t => {
+test('when imported should return a Object', t => {
   t.is(typeof spbus, 'object')
 })
