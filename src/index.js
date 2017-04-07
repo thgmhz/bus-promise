14230b0c0428ddc01dc19a1955e428df6bc26a06
= <thiagomunhozmedeiros@gmail.com>
2017-04-07 00:39:51 -0300

    Add babel on project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-const config = require('./config')
-const fetch = require('isomorphic-fetch')
+import fetch from 'isomorphic-fetch'
+import config from './config'
 
-const spbus = {
+export default {
   auth (token) {
     return fetch(`${config.endpoint}${config.auth}?token=${token}`, { method: 'POST' })
   }
 }
-
-module.exports = spbus
