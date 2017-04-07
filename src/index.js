ea09c91a84c6b62307dcdcf5c0c7e98ed690574a
= <thiagomunhozmedeiros@gmail.com>
2017-04-06 22:26:48 -0300

    Add spbus function

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const spbus = () => {}
+
+module.exports = spbus
---
+++ test/unit/index.test.js
@@ -0,0 +1,6 @@
+import test from 'ava'
+import spbus from '../../src'
+
+test('when imported should return a Function', t => {
+  t.is(typeof spbus, 'function')
+})
