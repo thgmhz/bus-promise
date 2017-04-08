87a19de9cb2129955a3cd686d9923e6b37396bb8
= <thiagomunhozmedeiros@gmail.com>
2017-04-08 00:07:19 -0300

    Update auth test

--- test/unit/auth.test.js
+++ test/unit/auth.test.js
@@ -1,6 +1,10 @@
 import test from 'ava'
 import spbus from '../../src'
 
+test('spbus should has the auth() method', t => {
+  t.is(typeof spbus.auth, 'function')
+})
+
 test('auth() should return an error when not receive a token', async t => {
   const error = await t.throws(spbus.auth)
   t.is(error.message, 'O token é obrigatório para autenticação.')
--- test/unit/index.test.js
+++
@@ -1,6 +0,0 @@
-import test from 'ava'
-import spbus from '../../src'
-
-test('when spbus is imported should return an object', t => {
-  t.is(typeof spbus, 'object')
-})
