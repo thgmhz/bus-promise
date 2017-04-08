a327b54f51c28eaf080b1370871d12d0608b2991
= <thiagomunhozmedeiros@gmail.com>
2017-04-08 03:05:42 -0300

    Improve auth test

--- test/unit/auth.test.js
+++ test/unit/auth.test.js
@@ -1,11 +1,10 @@
 import test from 'ava'
 import spbus from '../../src'
 
-test('spbus should has the auth() method', t => {
+test('spbus should has the auth method', t => {
   t.is(typeof spbus.auth, 'function')
 })
 
-test('auth() should return an error when not receive a token', async t => {
-  const error = await t.throws(spbus.auth)
-  t.is(error.message, 'O token é obrigatório para autenticação.')
+test('should return error when receive no token', t => {
+  t.throws(spbus.auth)
 })
