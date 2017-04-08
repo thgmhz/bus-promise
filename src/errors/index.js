45ab7e6501a7e325b7cc9551adb400b32fd6e801
= <thiagomunhozmedeiros@gmail.com>
2017-04-08 00:06:36 -0300

    Remove unused errors path

--- src/errors/index.js
+++
@@ -1,9 +0,0 @@
-const SpBusPromiseError = ({ message, type, errors }) => {
-  this.message = message
-  this.type = type
-  this.errors = errors
-}
-
-SpBusPromiseError.prototype = new Error()
-
-export default SpBusPromiseError
