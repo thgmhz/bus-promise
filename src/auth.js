ba19fb85ecec830f1c22198591ae7b8efeeb157f
= <thiagomunhozmedeiros@gmail.com>
2017-04-09 21:49:32 -0300

    Improve auth method code for better readable

--- src/auth.js
+++ src/auth.js
@@ -5,24 +5,24 @@ const handleError = err => {
   throw new Error(err)
 }
 
-const validateHttpStatus = res => {
-  if (res.status !== 200) handleError('Erro ao se conectar com o serviço da SPTrans.')
-  return res
-}
-
 const checkIfHasToken = token =>
   token || handleError('O token é obrigatório para autenticação.')
 
-const validateToken = res => {
-  if (!res.data) handleError('Token inválido.')
-  return res
-}
-
-const setCredentials = res => ({
-  credentials: res.headers['set-cookie'][0]
-})
-
 const fetchData = token => {
+  const validateHttpStatus = res => {
+    if (res.status !== 200) handleError('Erro ao se conectar com o serviço da SPTrans.')
+    return res
+  }
+
+  const validateToken = res => {
+    if (!res.data) handleError('Token inválido.')
+    return res
+  }
+
+  const setCredentials = res => ({
+    credentials: res.headers['set-cookie'][0]
+  })
+
   const config = {
     method: 'post',
     url: API.endpoint + API.auth.route,
