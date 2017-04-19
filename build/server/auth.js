a4a5ae0b9d40cd5fab7d63f3bfd1f720a96c2eb9
= <thiagomunhozmedeiros@gmail.com>
2017-04-18 21:00:03 -0300

    Update version to 1.0.3

--- build/server/auth.js
+++ build/server/auth.js
@@ -14,35 +14,36 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var isBrowser = typeof window !== 'undefined';
 
-var handleError = function handleError(err) {
+function handleError(err) {
   throw new Error(err);
-};
+}
 
-var handleResponse = function handleResponse(res) {
+function handleResponse(res) {
   if (isBrowser) return res.data;
   return res;
-};
+}
 
-var checkIfHasToken = function checkIfHasToken(token) {
-  return token || handleError('O token é obrigatório para autenticação.');
-};
+function checkIfHasToken(token) {
+  token || handleError('O token é obrigatório para autenticação.');
+  return token;
+}
 
-var validateHttpStatus = function validateHttpStatus(res) {
+function validateHttpStatus(res) {
   res.status === 200 || handleError('Erro ao se conectar com o serviço da SPTrans.');
   return res;
-};
+}
 
-var validateToken = function validateToken(res) {
+function validateToken(res) {
   res.data || handleError('Token inválido.');
   return res;
-};
+}
 
-var setCredentials = function setCredentials(res) {
+function setCredentials(res) {
   if (isBrowser) return res.auth[0];
   return res.headers['set-cookie'][0];
-};
+}
 
-var fetchData = function fetchData(token) {
+function fetchData(token) {
   var url = _constants.API.endpoint + _constants.API.auth.route;
 
   if (isBrowser) {
@@ -56,7 +57,7 @@ var fetchData = function fetchData(token) {
   };
 
   return (0, _axios2.default)(config).then(handleResponse).then(validateHttpStatus).then(validateToken).then(setCredentials);
-};
+}
 
 exports.default = function (token) {
   return Promise.resolve(token).then(checkIfHasToken).then(fetchData).catch(handleError);
