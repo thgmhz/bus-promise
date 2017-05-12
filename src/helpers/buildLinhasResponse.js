a4dc04887b57dc0db8ed1a699b7b0070f17e13c4
Thiago Medeiros <thiago.medeiros@pagar.me>
2017-05-12 18:17:39 -0300

    bug-fix: buildLinhasResponse

--- src/helpers/buildLinhasResponse.js
+++ src/helpers/buildLinhasResponse.js
@@ -8,14 +8,22 @@ export default async function buildLinhasResponse (linhas) {
       url: `${API.server}/trips/${tripId}`
     })
 
+  const parseShapeId = res =>
+    res.map(item => {
+      if (item.data[0]) return item.data[0].shape_id
+      return null
+    })
+
   const getShapesIds = tripsIds => {
     const promises = tripsIds.map(buildPromise)
-    return Promise.all(promises).then(res =>
-      res.map(item => item.data[0].shape_id))
+    return Promise.all(promises).then(parseShapeId)
   }
 
-  const tripsIds = linhas.map(item => `${item.Letreiro}-${item.Tipo}-${item.Sentido - 1}`)
+  const tripsIds = linhas.map(item =>
+    `${item.Letreiro}-${item.Tipo}-${item.Sentido - 1}`)
+
   const shapesIds = await getShapesIds(tripsIds)
+
   const response = linhas.map((item, key) => ({
     CodigoLinha: item.CodigoLinha,
     CodigoTrajeto: shapesIds[key],
