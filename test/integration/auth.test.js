8b2e6d20c3333626f9b679429f89111ee5665cce
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2017-06-27 17:20:46 -0300

    update: tests

--- test/integration/auth.test.js
+++ test/integration/auth.test.js
@@ -1,11 +1,11 @@
 import test from 'ava'
-import sptrans from '../../src'
+import bus from '../../src'
 
 // token livre para testes
 const TOKEN = '1e7c20905fe86990c5227e7e9f00002fe908d4d4dd4d7c0091032dacd2d0e07d'
 
 test('when authenticate should return credentials', async t => {
-  const auth = await sptrans.auth(TOKEN)
+  const auth = await bus.auth(TOKEN)
   t.true(typeof auth === 'string')
   t.true(auth.length > 0)
 })
--- test/unit/auth.test.js
+++ test/unit/auth.test.js
@@ -1,18 +1,18 @@
 import test from 'ava'
-import sptrans from '../../src'
+import bus from '../../src'
 
-test('sptrans should has the auth method', t => {
-  t.is(typeof sptrans.auth, 'function')
+test('bus-promise should has the auth method', t => {
+  t.is(typeof bus.auth, 'function')
 })
 
 test('when send no token should return error', async t => {
-  await sptrans.auth().catch(err => {
+  await bus.auth().catch(err => {
     t.true(err instanceof Error)
   })
 })
 
 test('when send a invalid token should return error', async t => {
-  await sptrans.auth('some-invalid-token').catch(err => {
+  await bus.auth('some-invalid-token').catch(err => {
     t.true(err instanceof Error)
   })
 })
