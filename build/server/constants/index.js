c2f8c3bf843c171def6e00bec3711b6bb889eddb
Thiago Santos <thia.mdossantos@gmail.com>
2017-07-14 13:30:32 -0300

    Add rollup

--- build/server/constants/index.js
+++ build/server/constants/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.API = undefined;
+
 var _api = require('./api');
 
 var _api2 = _interopRequireDefault(_api);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-module.exports = {
-  API: _api2.default
-};
+exports.API = _api2.default;
