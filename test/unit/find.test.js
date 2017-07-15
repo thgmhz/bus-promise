913fadcf3bc83a997c82044f77d705ed5b57d752
Thiago Santos <thia.mdossantos@gmail.com>
2017-07-15 15:58:28 -0300

    :bug: fix module problems

--- test/unit/find.test.js
+++ test/unit/find.test.js
@@ -1,5 +1,5 @@
 import test from 'ava'
-import bus from '../../src'
+import * as bus from '../../src'
 
 // token for tests
 const TOKEN = '2d5a4ee1443cb4047633305bf371c72213f6c3aefc9fe3362e42bccb3c01ebf4'
